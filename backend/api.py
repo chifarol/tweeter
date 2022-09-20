4f7c2298c644cee60d4cf2d52974c3b6891a2306
chifarol <ilodigwechinaza@gmail.com>
2022-09-20 12:29:50 -0700

    changed env variables

--- backend/api.py
+++ backend/api.py
@@ -332,6 +332,7 @@ def authstatus(req):
 import cloudinary
 from pathlib import Path
 import time
+import os
 import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
@@ -341,6 +342,6 @@ environ.Env.read_env(env.str(BASE_DIR, '/tweeter/.env'))
 def cloudinary_signature(req):
     params_to_sign = req.data['params']
     timestamp =  int((time.time()))
-    api_secret = env("CLOUD_SECRET")
+    api_secret = os.getenv("CLOUD_SECRET")
     signature = cloudinary.utils.api_sign_request({**params_to_sign, 'timestamp':timestamp}, api_secret)
     return JsonResponse({'signature':signature, "timestamp":timestamp})
