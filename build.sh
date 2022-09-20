5b81fa42c36ee12e9db4f9675da621a776578e73
chifarol <ilodigwechinaza@gmail.com>
2022-09-20 14:56:42 -0700

    modified pyproject.toml

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
-poetry install
+curl https://gist.githubusercontent.com/emilhe/0c7b1a33b2d02f17331242bf4fffd07c/raw/8da0665a58f469c980e7661d7f8c36f3bd3af992/strip_setuptools.py 11 | python - && poetry install
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
